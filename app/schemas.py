cc6413523ff2868d7b5bcf152ca0883fda5aef0e
johnwick22012026-source <johnwick22012026@gmail.com>
2026-07-30 16:11:41 +0530

    feat(TODOAPPL-32): Develop FastAPI Endpoints for Notes — app/schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class NoteBase(BaseModel):
+    text: str
+    is_completed: Optional[bool] = False
+
+class NoteCreate(NoteBase):
+    pass
+
+class Note(NoteBase):
+    id: int
+    created_at: datetime
+    completed_at: Optional[datetime] = None
+
+    class Config:
+        orm_mode = True
